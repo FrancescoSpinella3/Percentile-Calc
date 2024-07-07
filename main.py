c78650170d99eb60142c405e81dfc7599f7a3852
Francesco Spinella <francescospinella76@gmail.com>
2024-07-07 11:15:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,53 @@
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
-
-
+import customtkinter
 
 # PROGRAM TO PERCENTAGE CALCULATION
 def percentage_calculation():
     try:
-        starter_value = float(entry1.get()) # USER ENETER THE STARTER VALUE
-        final_value = float(entry2.get()) # USER ENETER THE FINAL VALUE
+        starter_value = float(entry1.get())  # USER ENETER THE STARTER VALUE
+        final_value = float(entry2.get())  # USER ENETER THE FINAL VALUE
 
-        result = (final_value / starter_value) * 100 - 100 # CALCULATE THE RESULT
-        label_result.config(text= f"Result: {result:.2f}")
-        #return input("Press Enter to exit.")
+        result = (final_value / starter_value) * 100 - 100  # CALCULATE THE RESULT
+        label_result.config(text=f"Result: {result:.2f}%")
+
     except ValueError as error:
-        label_result.config(text= "Error: Enter a valid value")
-        #return input("Press Enter to exit. ")
+        label_result.config(text="Error: Enter a valid value")
 
-# CREATING MAIN WINDOW 
-root = tk.Tk()
+# CREATING MAIN WINDOW
+root = customtkinter.CTk()
+root.resizable(width=False, height=False)
+root.eval('tk::PlaceWindow . center')
 root.title("Percentile calculation")
-root.geometry("300x200")
+root.geometry("200x250")
 
-#CREATING WIDGET
-label1 = tk.Label(root, text= "First value")
-label1.pack(pady= 5)
+# CREATING CENTRAL FRAME
+central_frame = tk.Frame(root) # CAMBIARE BACKGROUND COLOR NEL FRAME E RENDERLO UGUALE AL RESTO: GRIGIO SCURO
+central_frame.pack(expand=True)
 
-entry1 = tk.Entry(root)
-entry1.pack(pady= 5)
+# CREATING WIDGETS INSIDE CENTRAL FRAME
+label1 = tk.Label(central_frame, text="First value")
+label1.pack(pady=2)
 
-label2 = tk.Label(root, text= "Second value")
-label2.pack(pady= 5)
+entry1 = tk.Entry(central_frame)
+entry1.pack(pady=5)
 
-entry2 = tk.Entry(root)
-entry2.pack(pady= 5)
+label2 = tk.Label(central_frame, text="Second value")
+label2.pack(pady=2)
+
+entry2 = tk.Entry(central_frame)
+entry2.pack(pady=5)
 
 # BUTTON TO DO THE CALCULATION
-button = tk.Button(root, text= "Calculate", command= percentage_calculation)
-button.pack(pady= 10)
+button = customtkinter.CTkButton(master=central_frame, text="Calculate", command=percentage_calculation)
+button.pack(pady=5)
 
-# LABEL TO SHOW TH RESULT
-label_result = tk.Label(root, text= "Result: ")
-label_result.pack(pady= 5)
+# LABEL TO SHOW THE RESULT
+label_result = tk.Label(central_frame, text="Result: ")
+label_result.pack(pady=8)
+
 root.mainloop()
-
-
-#print(percentage_calculation())
 
 
 
